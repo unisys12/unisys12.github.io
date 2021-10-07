075c46caf2b7f6b94919d21f760882ddf688ce03
Phillip Jackson <unisys12@gmail.com>
2021-10-07 17:27:49 -0500

    add syntax highlighting

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,11 @@
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.setTemplateFormats(["md", "njk"]);
 
   eleventyConfig.addPassthroughCopy("./src/assets/css/styles.css");
+
+  eleventyConfig.addPlugin(syntaxHighlight);
 
   return {
     dir: {
