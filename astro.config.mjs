3118a7781fdab014b59216ced6d002602ef3d368
unisys12 <unisys12@gmail.com>
2024-07-11 20:07:09 -0500

    configure shiki to use material-theme-darker

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,11 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://unisys12.github.io'
+    site: 'https://unisys12.github.io',
+    markdown: {
+        shikiConfig: {
+            theme: 'material-theme-darker',
+            transformers: [],
+        }
+    }
 });
